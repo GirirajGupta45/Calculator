127a6e7a8008654038cac23a22baa311cffb4efc
Farhana Khatoon <farhana30092k1@gmail.com>
2024-05-03 23:06:05 +0530

    added maths file

---
+++ src/main/java/com/java/maths.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class maths
+ */
+@WebServlet("/maths")
+public class maths extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		pw.println("let's study maths operations with giriraj, farhana and akansha.");
+	}
+
+}
